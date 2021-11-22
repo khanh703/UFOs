2f4a3acb44cd79c19443da6807b30f8146ca8ca0
khanh703 <khanh703@gmail.com>
2021-11-22 16:14:50 -0700

    Module 11.6.3

---
+++ static/app.js
@@ -0,0 +1,37 @@
+const tableData = data;
+
+function buildTable(data) {
+    var tbody = d3.select("#tbody")
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+
+        let row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((val) => {
+
+            let cell = row.append("td");
+
+            cell.text(val);
+        });
+
+    });
+
+}
+
+
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData);
+}
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+
+// Build the table when the page loads
+buildTable(tableData);
